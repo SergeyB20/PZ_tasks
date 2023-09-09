e16ef286a715dbebff9c23adf5e4496515d81b20
Sergey <125741718+SergeyB20@users.noreply.github.com>
2023-09-09 19:16:35 +0300

    solved

--- PZ_2_13.py
+++ PZ_2_13.py
@@ -1,24 +1,27 @@
 def solve(number):
     # check on minus
- if str(number)[0] == "-":
-    spl = str(number).split('-')
-    resOfSpl = spl[1]
-# revers of number
-    if len(resOfSpl) != 2:
-        print('Number is not two-digit')
-        start()
-    else:
-        print("Your reversed number:", '-'+resOfSpl[::-1])
+    if str(number)[0] == "-":
+        spl = str(number).split('-')
+        ResSpl = spl[1]
+        # revers of number
+        if len(ResSpl) != 2:
+            print('Number is not two-digit')
+            start()
+        else:
+            print("Your reversed number:", '-' + ResSpl[::-1])
 
-# revers of number without minus
- else:
-    if len(str(number)) != 2:
-        print('Number is not two-digit or other')
-        start()
+    # revers of number without minus
     else:
-        print("Your reversed number:", str(number)[::-1])
+        if len(str(number)) != 2:
+            print('Number is not two-digit or other')
+            start()
+        else:
+            print("Your reversed number:", str(number)[::-1])
+
 
 def start():
     solve(input('Enter two-digit number:'))
 
+
 start()
+
